7223cff626226444ec728bc3c08feeddb239390c
Jason Winget <jwinget@gmail.com>
2023-05-12 13:55:51 -0400

    Converting to package

--- spacedash/app.R
+++ spacedash/app.R
@@ -34,34 +34,45 @@ ui <- cartridge(
 
 server <- function(input, output, session) {
   source(here::here("R", "funs.R"))
-  
+
   future::plan("multisession", workers = 5)
-  
+
   char_info <- reactive({
     invalidateLater(60006, session)
     agent_info(token, base_url)
   })
-  
+
   output$credit_balance <- renderText({
     char_info()$data$credits})
-  
+
   s_info <- reactive({
     invalidateLater(15005, session)
     ships(token, base_url)$data
   })
-    
-  ship_summary <- reactive({tibble::tibble(
+
+  ship_summary <- reactive({
+    ship_icons <- c(
+      "FRAME_FRIGATE" = '<img src="img/craft_speederB_SE.png"></img>',
+      "FRAME_DRONE" = '<img src="img/craft_miner_SE.png"></img>'
+    )
+
+    ship_types <- s_info()$frame$symbol
+
+    tibble::tibble(
       symbol = s_info()$symbol,
+      #ship_type = ship_icons[ship_types],
       current_waypoint = s_info()$nav$waypointSymbol,
       waypoint_type = s_info()$nav$route$destination$type,
       status = s_info()$nav$status,
-      fuel = s_info()$fuel$current
+      fuel = s_info()$fuel$current,
+      cargo_load = glue::glue("{round(100*s_info()$cargo$units / s_info()$cargo$capacity, 2)}%")
     )
   })
-  
+
   output$ship_status <- renderTable({
-    ship_summary()})
-  
+    ship_summary()
+    })
+
   contract_summary <- reactive({
     invalidateLater(2E5+25, session)
     my_contracts <- contracts(token, base_url)$data
@@ -72,35 +83,35 @@ server <- function(input, output, session) {
       percent_complete = 100 * as.numeric(d[["unitsFulfilled"]]) / as.numeric(d[["unitsRequired"]])
     )
   })
-  
+
   output$contracts <- renderTable({
     contract_summary()})
-  
+
   # Ad-hoc commands
   output$ship_select_ui <- renderUI({
     selectInput("ship_select", "Select ship:", choices = ship_summary()$symbol)
   })
-  
+
   observeEvent(input$dock, {
     message(glue::glue("Docking {input$ship_select}!"))
     dock(token, base_url, input$ship_select)
   })
-  
+
   observeEvent(input$refuel, {
     message(glue::glue("Gassing up {input$ship_select}"))
     refuel(token, base_url, input$ship_select)
   })
-  
+
   observeEvent(input$orbit, {
     message(glue::glue("{input$ship_select} cleared for departure"))
     orbit(token, base_url, input$ship_select)
   })
-  
+
   observeEvent(input$extract, {
     message(glue::glue("{input$ship_select} getting to work"))
     extract_until_full(token, base_url, input$ship_select)
   })
-  
+
   observeEvent(input$unload, {
     message(glue::glue("{input$ship_select} heading to market"))
     unload_cargo(token, base_url, input$ship_select)
@@ -110,12 +121,12 @@ server <- function(input, output, session) {
     message(glue::glue("All ships working against contract {contract_summary()$id[[1]]}"))
     run_swarm(token, base_url)
   })
-  
+
   observeEvent(input$navigate, {
     message(glue::glue("{input$ship_select} navigating to {input$waypoint}"))
     navigate(token, base_url, input$ship_select, input$waypoint)
   })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
